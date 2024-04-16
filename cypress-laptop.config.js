57920adad43ac638ab84a5d7844f99c91c3814ea
Valeria Spektor <vlapin84@gmail.com>
2024-04-16 16:09:04 +0300

    udated test for 9.2.1

--- cypress-laptop.config.js
+++ cypress-laptop.config.js
@@ -4,8 +4,9 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
-    specPattern: "**/laptop/*.js", // указываем путь к тестам для лэптопа
+    specPattern: "**/laptop/*.js",
     baseUrl: "https://sqlverifier-staging-08050d656f7a.herokuapp.com",
+    projectId: "fs2ut8", // Добавляем projectId
     setupNodeEvents(on, config) {}
   }
 });
--- cypress-monitor.config.js
+++ cypress-monitor.config.js
@@ -4,12 +4,10 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    specPattern: "**/monitor/*.js", // указываем путь к тестам для монитора
-    projectId: "fs2ut8",
+    specPattern: "**/monitor/*.js",
     baseUrl: "https://sqlverifier-staging-08050d656f7a.herokuapp.com",
-    setupNodeEvents(on, config) {
-      // реализация обработчиков событий узла здесь
-    },
+    projectId: "fs2ut8", // Добавляем projectId
+    setupNodeEvents(on, config) {}
   },
   chromeWebSecurity: false,
   taskTimeout: 10000
