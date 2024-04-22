a8e907151586683d3b00f1dc6dda9b7748870091
Valeria Spektor <vlapin84@gmail.com>
2024-04-22 13:50:00 +0300

    created jira credentials for 12.1.1

---
+++ jira_credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'https://valeria-spektor.atlassian.net/',
+    username: 'vlapin84@gmail.com',
+    password: '542073vl',
+    apiVersion: '2',
+    strictSSL: true // установите в false, если ваш сервер Jira использует самоподписанный SSL-сертификат
+};
