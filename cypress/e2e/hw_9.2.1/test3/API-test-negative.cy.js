d8ad2233461bc84523e332c6114879ccaeed7caf
Valeria Spektor <vlapin84@gmail.com>
2024-04-22 13:50:46 +0300

    added negative new test for 12.1.1

---
+++ cypress/e2e/hw_9.2.1/test3/API-test-negative.cy.js
@@ -0,0 +1,107 @@
+// Create new user with empty login
+describe('Create new user with empty login', () => {
+    it('should return status code 401', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/register',
+            headers: {
+                'accept': '*/*',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbl9hdXRvbWF0aW9uIiwiZXhwIjoxNzA4NzkyNDA4LCJhdXRoIjoiUk9MRV9BRE1JTiIsImlhdCI6MTcwODcwNjAwOH0.hm8rWyzSBCDNMVirgQQTai-Yyo4SpfTwkcVF7SzyZ8zTMldAlLxkGGt8JCkkB_nazTwH8G2DgqTl3m6rvi6Obw'
+            },
+            body: {
+                "id": 0,
+                "login": " ",
+                "firstName": "Sofia",
+                "lastName": "Spektor",
+                "email": "sofiaspektor22@gmail.com",
+                "imageUrl": "string",
+                "activated": true,
+                "langKey": "en",
+                "createdBy": "string",
+                "createdDate": "2024-02-22T12:12:59.374Z",
+                "lastModifiedBy": "string",
+                "lastModifiedDate": "2024-02-22T12:12:59.374Z",
+                "authorities": [
+                    "ROLE_STUDENT"
+                ],
+                "password": "542073"
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(401);
+        });
+    });
+});
+
+// Create a new user with login consisting of digits
+describe('Create a new user with login consisting of digits', () => {
+    it('should return status code 401', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/register',
+            headers: {
+                'accept': '*/*',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbl9hdXRvbWF0aW9uIiwiZXhwIjoxNzA4NzkyNDA4LCJhdXRoIjoiUk9MRV9BRE1JTiIsImlhdCI6MTcwODcwNjAwOH0.hm8rWyzSBCDNMVirgQQTai-Yyo4SpfTwkcVF7SzyZ8zTMldAlLxkGGt8JCkkB_nazTwH8G2DgqTl3m6rvi6Obw'
+            },
+            body: {
+                "id": 0,
+                "login": "12345 ",
+                "firstName": "Sofia",
+                "lastName": "Spektor",
+                "email": "sofiaspektor212@gmail.com",
+                "imageUrl": "string",
+                "activated": true,
+                "langKey": "en",
+                "createdBy": "string",
+                "createdDate": "2024-02-22T12:12:59.374Z",
+                "lastModifiedBy": "string",
+                "lastModifiedDate": "2024-02-22T12:12:59.374Z",
+                "authorities": [
+                    "ROLE_STUDENT"
+                ],
+                "password": "542073"
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(401);
+        });
+    });
+});
+
+// User registration with login length more than 50 letters
+describe('User registration with login length more than 50 letters', () => {
+    it('should return status code 401', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/register',
+            headers: {
+                'accept': '*/*',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer your_access_token_here'
+            },
+            body: {
+                "id": 0,
+                "login": "SofiaSofiaSofiaSofiaSofiaSofiaSofiaSofiaSofiaSofiaS ",
+                "firstName": "Sofia",
+                "lastName": "Spektor",
+                "email": "sofiaspektor212@gmail.com",
+                "imageUrl": "string",
+                "activated": true,
+                "langKey": "en",
+                "createdBy": "string",
+                "createdDate": "2024-02-22T12:12:59.374Z",
+                "lastModifiedBy": "string",
+                "lastModifiedDate": "2024-02-22T12:12:59.374Z",
+                "authorities": [
+                    "ROLE_STUDENT"
+                ],
+                "password": "542073"
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(401);
+        });
+    });
+});
